5297f8fd4fec48dacff046ef20ca0378ddcfa5c5
EasonMa1123 <yoyomam1123@gmail.com>
2025-02-18 23:03:02 +0000

    add basic chat room

---
+++ static/index_event.js
@@ -0,0 +1,31 @@
+
+
+
+function setting_on() {
+    document.getElementById("overlay-setting").style.display = "block";
+    menu_close();
+
+}
+  
+function setting_off() {
+    document.getElementById("overlay-setting").style.display = "none";
+} 
+
+
+function menu_open() {
+    document.getElementById('sidebar').style.display = "block";
+    document.getElementById('Username-display').innerHTML = sessionStorage.getItem("Username")
+
+
+}
+
+function menu_close() {
+    document.getElementById('sidebar').style.display = "None";
+    document.getElementById('Username-display').innerHTML = ""
+
+
+}
+
+
+
+
---
+++ static/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+// index.html
+
+function check_invalid_enter(){
+    if(sessionStorage.getItem("Username") == null){
+        alert("What are you doing in here,this is not your place,leave! Thank You :)")
+        logout()
+    }
+}
+
+
+
+
+
+
+function logout(){
+    document.location.href = "/login";
+    sessionStorage.setItem("",Username)
+    sessionStorage.setItem("",Password)
+}
