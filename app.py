5b98f7e2fd49311b155ae74ab6a76065bb819447
EasonMa1123 <yoyomam1123@gmail.com>
2025-02-18 22:38:20 +0000

    add login html backend functions

--- app.py
+++ app.py
@@ -13,6 +13,31 @@ def index():
 def home():
     return render_template('login.html')
 
+@app.route('/insertNewUser', methods = ['POST'])
+def insert_new_user():
+    userName = request.form['userName']
+    password = request.form['Password']
+    Email = request.form['Email']
+    if DataRecord().check_user(userName):
+        return jsonify({"Feedback":"Invalid Username,This Username had been used "}) 
+    else:
+        DataRecord().insert_new_user(userName,password,Email)
+        return jsonify({"Feedback":"Success"})
+
+@app.route('/password_strength', methods = ['POST'])
+def password_strength_check():
+    password = request.form['Password']
+    score = password_strength_checker().password_check(password)
+    return jsonify({"score":score})
+
+@app.route('/access_account_detail', methods = ['POST'])
+def access_account_detail():
+    Username = request.form['Username']
+    return jsonify({"ID":DataRecord().access_account(Username)[0],
+                    "Username":DataRecord().access_account(Username)[1],
+                    "Password":DataRecord().access_account(Username)[2],
+                    "email":DataRecord().access_account(Username)[3]})
+
 @app.route('/send', methods=['POST'])
 def send():
     username = request.form.get('username')
